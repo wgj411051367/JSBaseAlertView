70afdb51fd6c9cf301515a3edf7594f7bd0971f4
万冠杰 <411051367@qq.com>
2021-11-01 15:55:27 +0800

    上传工程

---
+++ Example/Pods/Target Support Files/JSBaseAlertView/JSBaseAlertView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JSBaseAlertView : NSObject
+@end
+@implementation PodsDummy_JSBaseAlertView
+@end
---
+++ Example/Pods/Target Support Files/Pods-JSBaseAlertView_Example/Pods-JSBaseAlertView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JSBaseAlertView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_JSBaseAlertView_Example
+@end
